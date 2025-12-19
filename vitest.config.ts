acc273064180d20dcb79abd172e78aa36e025148
Nathan Darcy <nathandarcy33@gmail.com>
2025-12-19 13:54:53 -0600

    main: cleanup some missed lint rules - Nathan

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
-    setupFiles: "./src/setupTests.ts",
+    setupFiles: './src/setupTests.ts',
     css: true,
     typecheck: {
-      tsconfig: "./tsconfig.vitest.json",
+      tsconfig: './tsconfig.vitest.json',
     },
   },
-});
+})
