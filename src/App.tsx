a3dfaa1f1c5efd16e272ab1fc5b2b9b98690e561
Nathan Darcy <nathandarcy33@gmail.com>
2025-12-22 10:28:13 -0600

    deletion and priroity coloring - Nathan

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,24 @@ import NoteList from './modules/notes/components/NoteList'
 export default function App() {
   const [notes, setNotes] = useState<Note[]>([])
 
+  function handleDeleteNote(id: number) {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete this note?'
+    )
+
+    if (!confirmDelete) {
+      return
+    }
+
+    setNotes(notes.filter((note) => id !== note.id))
+  }
+
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">Notes App</h2>
 
       <NoteForm notes={notes} setNotes={setNotes} />
-      <NoteList notes={notes} />
+      <NoteList notes={notes} deleteNote={handleDeleteNote} />
     </div>
   )
 }
--- src/modules/notes/components/NoteList.tsx
+++ src/modules/notes/components/NoteList.tsx
@@ -2,11 +2,22 @@ import type { Note } from '../types/note.type'
 
 export type NoteListProps = {
   notes: Note[]
+  deleteNote: (id: number) => void
 }
 
-export default function NoteList({ notes }: NoteListProps) {
+export default function NoteList({ notes, deleteNote }: NoteListProps) {
   if (!notes.length) {
     return <p className="text-center text-gray-500">No notes yet</p>
+  }
+
+  function getStyleByPriority(priority: 'High' | 'Medium' | 'Low') {
+    if (priority === 'High') {
+      return 'red'
+    }
+    if (priority === 'Medium') {
+      return 'orange'
+    }
+    return 'green'
   }
 
   return (
@@ -15,6 +26,7 @@ export default function NoteList({ notes }: NoteListProps) {
         <div
           key={note.id}
           className="p-4 bg-white rounded-lg shadow-md border-l-4"
+          style={{ borderLeftColor: getStyleByPriority(note.priority) }}
         >
           <h3 className="text-lg font-bold">{note.title}</h3>
           <p className="text-sm text-gray-600">
@@ -26,6 +38,15 @@ export default function NoteList({ notes }: NoteListProps) {
             {note.priority}
           </p>
           <p className="mt-2">{note.description}</p>
+
+          <button
+            onClick={() => {
+              deleteNote(note.id)
+            }}
+            className="mt-3 text-red-500 cursor-pointer transition hover:text-red-700"
+          >
+            Delete
+          </button>
         </div>
       ))}
     </div>
